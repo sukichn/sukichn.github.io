eaaf485b6c65fde43e1cda49c556b816f79ea13a
SZYC (Suki Chan) <SZYC@novonordisk.com>
2024-09-18 16:19:26 -0400

    changed default potion generator images, added tries counter, added Potion IV as an option

--- veridian.js
+++ veridian.js
@@ -30,11 +30,19 @@ let fortuneMessage = document.getElementById("fortuneMessage");
 const potionImages = [
     "Resources/css/Images/Potion_I.png",
     "Resources/css/Images/Potion_II.png",
-    "Resources/css/Images/Potion_III.png"
+    "Resources/css/Images/Potion_III.png",
+    "Resources/css/Images/Potion_IV.png"
   ];
-  
+
+  // Define a variable to keep track of the number of tries
+let tryCount = 0;
   // Function to generate a random potion
   function generateRandomPotion() {
+ // Check if the number of tries is less than 10
+ if (tryCount < 10 && !fortuneButton.disabled) {
+    // Increment the try count
+    tryCount++;
+    document.getElementById("tryCountDisplay").textContent = "Number of tries: " + tryCount;
     // Generate random potion indices
     const randomIndex1 = Math.floor(Math.random() * potionImages.length);
     const randomIndex2 = Math.floor(Math.random() * potionImages.length);
@@ -44,15 +52,30 @@ const potionImages = [
     potionImage1.src = potionImages[randomIndex1];
     potionImage2.src = potionImages[randomIndex2];
     potionImage3.src = potionImages[randomIndex3];
-  
+
     // Check if all potion images match
     if (randomIndex1 === randomIndex2 && randomIndex2 === randomIndex3) {
-      fortuneMessage.innerHTML="Congratulations! You've won a potion!";
-    } else {
-    fortuneMessage.innerHTML="Better luck next time! No matching potions this time.";
-    }
-  }
-  
+        if (randomIndex1 === 0) {
+          fortuneMessage.innerHTML = "Congratulations! You've won Potion I!";
+        } else if (randomIndex1 === 1) {
+          fortuneMessage.innerHTML = "Congratulations! You've won Potion II!";
+        } else if (randomIndex1 === 2) {
+          fortuneMessage.innerHTML = "Congratulations! You've won Potion III!";
+        } else {
+            fortuneMessage.innerHTML = "Congratulations! You've won Potion IV!";
+          }
+    // Disable the button after winning
+    fortuneButton.disabled = true;
+} else {
+  fortuneMessage.innerHTML = "Better luck next time! No matching potions this time.";
+}
+} else {
+fortuneMessage.innerHTML = "You've reached the maximum number of tries.";
+}
+}
+
+
+
   // Event listener for the "Try your luck" button
   fortuneButton.addEventListener("click", generateRandomPotion);
 
