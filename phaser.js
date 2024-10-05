bc7a36368fba3951ce719b51a37ef018a4826cca
Suki Chan <sukihchn@gmail.com>
2024-10-04 20:10:04 -0400

    added background images to BaseScene

--- phaser.js
+++ phaser.js
@@ -4,25 +4,49 @@ var swipeDirection;
 let swipeStartX = 0;
 let swipeStartY = 0;
 
-// Phaser Start Scene
-class StartScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'StartScene' });
+class BaseScene extends Phaser.Scene {
+    constructor(key) {
+        super({ key });
     }
 
     preload() {
-        // Load any assets required for the start scene
-        this.load.image('bg', 'https://static.vecteezy.com/system/resources/previews/024/387/936/large_2x/fantasy-magical-enchanted-fairy-tale-landscape-fabulous-fairytale-garden-mysterious-background-and-glowing-in-night-magical-fantasy-fairy-tale-scenery-night-in-a-forest-generate-ai-free-photo.jpg');
+        // Load common assets
+        this.load.image('bg', 'https://raw.githubusercontent.com/devshareacademy/phaser-3-typescript-games-and-examples/refs/heads/main/examples/parallax-scrolling-background/public/assets/images/background.png');      
+        this.load.image('fog', 'https://raw.githubusercontent.com/devshareacademy/phaser-3-typescript-games-and-examples/refs/heads/main/examples/parallax-scrolling-background/public/assets/images/fog.png');      
+        this.load.image('trees', 'https://raw.githubusercontent.com/devshareacademy/phaser-3-typescript-games-and-examples/refs/heads/main/examples/parallax-scrolling-background/public/assets/images/trees.png');      
+        this.load.image('foreground', 'https://raw.githubusercontent.com/devshareacademy/phaser-3-typescript-games-and-examples/refs/heads/main/examples/parallax-scrolling-background/public/assets/images/foreground.png');      
+    }
+
+    createBackground() {
+        gameState.background = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'bg').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.trees = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'trees').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.fog = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'fog').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.foreground = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'foreground').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+    }
+
+    updateBackground() {
+        gameState.background.tilePositionX += 0.1;
+        gameState.trees.tilePositionX += 0.14;
+        gameState.foreground.tilePositionX += 0.2;
+        gameState.fog.tilePositionX += 0.7;
+    }
+}
+
+class StartScene extends BaseScene {
+    constructor() {
+        super('StartScene');
+    }
+
+    preload() {
+        super.preload();
         this.load.image('startButton', 'https://raw.githubusercontent.com/sukichn/sukichn.github.io/refs/heads/main/Resources/css/Images/Potion_I.png'); // Replace with the actual path to your start button image
     }
 
     create() {
-        // Add a background
-        gameState.background = this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(0.5);
-        gameState.background.setDepth(0); // Set the depth of the background
+        super.createBackground();
 
         // Add a title
-        this.add.text(800, 250, 'Start Game', { fontSize: '58px', fill: '#ffffff', fontFamily: 'Work Sans'}).setOrigin(0.5).setDepth(1);
+        this.add.text(800, 250, 'Start Game', { fontSize: '58px', fill: '#ffffff', fontFamily: 'Work Sans' }).setOrigin(0.5).setDepth(1);
 
         // Add a start button
         const startButton = this.add.image(800, 500, 'startButton').setInteractive().setDepth(1).setScale(0.1);
@@ -32,18 +56,22 @@ class StartScene extends Phaser.Scene {
         });
 
         // Add instructions or other UI elements as needed
-        this.add.text(800, 750, 'Click to start!', { fontSize: '32px', fill: '#ffffff', fontFamily: 'Work Sans'}).setOrigin(0.5).setDepth(1);
+        this.add.text(800, 750, 'Click to start!', { fontSize: '32px', fill: '#ffffff', fontFamily: 'Work Sans' }).setOrigin(0.5).setDepth(1);
+    }
+
+    update() {
+        super.updateBackground();
     }
 }
 
 // Phaser Game Scene
-class GameScene extends Phaser.Scene {
+class GameScene extends BaseScene {
     constructor() {
-        super({ key: 'GameScene' });
+        super('GameScene');
     }
 
     preload() {
-        this.load.image('bg', 'https://static.vecteezy.com/system/resources/previews/024/387/936/large_2x/fantasy-magical-enchanted-fairy-tale-landscape-fabulous-fairytale-garden-mysterious-background-and-glowing-in-night-magical-fantasy-fairy-tale-scenery-night-in-a-forest-generate-ai-free-photo.jpg');      
+        super.preload();
         this.load.image('cave', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/cave_background.png');
         this.load.image('platform', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/platform.png');
         this.load.spritesheet('codey', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/codey_sprite.png', { frameWidth: 72, frameHeight: 90 });
@@ -52,14 +80,11 @@ class GameScene extends Phaser.Scene {
         this.load.image('leftButton', 'Resources/css/Images/.png'); // Replace with the actual path to your start button image
         this.load.image('upButton', 'Resources/css/Images/.png'); // Replace with the actual path to your start button image
         this.load.image('rightButton', 'Resources/css/Images/.png'); // Replace with the actual path to your start button image
-
     }
 
     create() {
-        gameState.background = this.add.tileSprite(-1500, 0, this.cameras.main.width*10, this.cameras.main.height*3, 'bg').setOrigin(0, 0).setScale(0.5);
-
+        super.createBackground();
         gameState.active = true;
-    
     
         const platforms = this.physics.add.staticGroup();
         const platPositions = [
@@ -70,8 +95,7 @@ class GameScene extends Phaser.Scene {
           platforms.create(plat.x, plat.y, 'platform')
         });
     
-        gameState.player = this.physics.add.sprite(50, 500, 'codey').setScale(.8)
-    
+        gameState.player = this.physics.add.sprite(50, 500, 'codey').setScale(.8);
         this.physics.add.collider(gameState.player, platforms);
         gameState.player.setCollideWorldBounds(true);
     
@@ -92,8 +116,6 @@ class GameScene extends Phaser.Scene {
         });
     
         gameState.enemy = this.physics.add.sprite(480, 300, 'snowman');
-    
-        platforms
         this.physics.add.collider(gameState.enemy, platforms);
     
         this.anims.create({
@@ -114,7 +136,7 @@ class GameScene extends Phaser.Scene {
           this.input.on('pointerup', () => {
             this.anims.resumeAll();
             this.scene.restart();
-          })
+          });
         });
     
         gameState.exit = this.physics.add.sprite(50, 142, 'exit');
@@ -136,9 +158,8 @@ class GameScene extends Phaser.Scene {
           this.input.on('pointerup', () => {
             this.anims.resumeAll();
             this.scene.restart();
-          })
-        })
-    
+          });
+        });
     
         gameState.enemy.move = this.tweens.add({
           targets: gameState.enemy,
@@ -148,7 +169,7 @@ class GameScene extends Phaser.Scene {
           repeat: -1,
           yoyo: true,
           onRepeat: growSnowman
-        })
+        });
         
         let scaleChange = 1.1;
         function growSnowman() {
@@ -159,83 +180,66 @@ class GameScene extends Phaser.Scene {
           }
         }
 
-        const leftButton = this.add.image(0, 700, 'leftButton').setInteractive().setDepth(1);
-        const upButton = this.add.image(50, 700, 'leftButton').setInteractive().setDepth(1);
-        const rightButton = this.add.image(100, 700, 'leftButton').setInteractive().setDepth(1);
-
-
         // Set the camera to follow the player only along the x-axis
         this.cameras.main.startFollow(gameState.player, true);
 
+        // Touch input settings for left/right press detection
+        this.input.on('pointerdown', function (pointer) {
+            if (pointer.x < this.scale.width / 2) {
+                // Left side of the screen
+                gameState.leftPressed = true;
+                gameState.rightPressed = false;
+            } else {
+                // Right side of the screen
+                gameState.rightPressed = true;
+                gameState.leftPressed = false;
+            }
 
-      }
+            // Store the starting positions for swipe detection
+            isClicking = true;
+            swipeStartY = pointer.y;
+        }, this);
 
+        this.input.on('pointerup', function (pointer) {
+            gameState.leftPressed = false;
+            gameState.rightPressed = false;
+
+            // Capture the end position of the swipe
+            if (isClicking) {
+                let swipeEndY = pointer.y;
+                let deltaY = swipeEndY - swipeStartY;
+
+                if (Math.abs(deltaY) >= 50 && deltaY < 0) {
+                    swipeDirection = 'up';
+                }
+
+                isClicking = false;
+            }
+        }, this);
+    }
 
     update() {
-        
+        // Update background positions for parallax effect
+        gameState.background.tilePositionX += 0.1;
+        gameState.trees.tilePositionX += 0.14;
+        gameState.foreground.tilePositionX += 0.2;
+        gameState.fog.tilePositionX += 0.7;
 
         if (gameState.active) {
-            // Touch input settings
-            this.input.on('pointerdown', function (pointer) {
-                if (pointer.x < this.scale.width / 2) {
-                    // Left side of the screen
-                    gameState.leftPressed = true;
-                    gameState.rightPressed = false;
-                } else {
-                    // Right side of the screen
-                    gameState.rightPressed = true;
-                    gameState.leftPressed = false;
-                }
-            }, this);
-        
-            this.input.on('pointerup', function (pointer) {
-                gameState.leftPressed = false;
-                gameState.rightPressed = false;
-            }, this);
-        
-            this.input.on('pointerdown', function (pointer) {
-                if (pointer.y < this.scale.height / 2) {
-                    // Top half of the screen
-                    gameState.upPressed = true;
-                }
-            }, this);
-        
-            this.input.on('pointerup', function (pointer) {
-                gameState.upPressed = false;
-            }, this);
-        
             // Handle movement based on touch input or keyboard input
             if (gameState.cursors.left.isDown || gameState.leftPressed) {
                 gameState.player.setVelocityX(-360);
                 gameState.player.anims.play('run', true);
-            } else if (gameState.cursors.right.isDown || gameState.rightPressed) {
-                gameState.player.setVelocityX(360);
-                gameState.player.anims.play('run', true);
-            } else {
-                gameState.player.setVelocityX(0);
-                gameState.player.anims.play('idle', true);
-            }
-        
-            if ((gameState.cursors.up.isDown || gameState.upPressed) && gameState.player.body.touching.down) {
-                gameState.player.setVelocityY(-800);
-            }
-    
-            // Handle movement based on swipe gestures or keyboard input
-            if (gameState.cursors.left.isDown || gameState.leftPressed) {
-                gameState.player.setVelocityX(-360);
-                gameState.player.anims.play('run', true);
                 gameState.player.flipX = true;
-                swipeDirection = '';  // Reset swipe direction after handling
             } else if (gameState.cursors.right.isDown || gameState.rightPressed) {
                 gameState.player.setVelocityX(360);
                 gameState.player.anims.play('run', true);
                 gameState.player.flipX = false;
-                swipeDirection = '';  // Reset swipe direction after handling
             } else {
                 gameState.player.setVelocityX(0);
                 gameState.player.anims.play('idle', true);
             }
-    
+
             if ((gameState.cursors.up.isDown || swipeDirection === 'up') && gameState.player.body.touching.down) {
                 gameState.player.setVelocityY(-800);
                 swipeDirection = '';  // Reset swipe direction after handling
