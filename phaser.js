fce9ae7cb35db9bcc858bec55abd44a6157af691
Suki Chan <sukihchn@gmail.com>
2024-10-05 20:33:51 -0400

    added coins to platforms and second enemy sprite

--- phaser.js
+++ phaser.js
@@ -18,10 +18,10 @@ class BaseScene extends Phaser.Scene {
     }
 
     createBackground() {
-        gameState.background = this.add.tileSprite(0, 55, this.cameras.main.width, this.cameras.main.height, 'bg').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
-        gameState.trees = this.add.tileSprite(0, 55, this.cameras.main.width, this.cameras.main.height, 'trees').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
-        gameState.fog = this.add.tileSprite(0, 55, this.cameras.main.width, this.cameras.main.height, 'fog').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
-        gameState.foreground = this.add.tileSprite(0, 55, this.cameras.main.width, this.cameras.main.height, 'foreground').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.background = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'bg').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.trees = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'trees').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.fog = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'fog').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
+        gameState.foreground = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, 'foreground').setOrigin(0, 0).setScrollFactor(0).setScale(4.5);
     }
 
     updateBackground() {
@@ -82,7 +82,7 @@ class GameScene extends BaseScene {
         this.load.spritesheet('codey', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/codey_sprite.png', { frameWidth: 72, frameHeight: 90 });
         this.load.spritesheet('snowman', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/snowman.png', { frameWidth: 50, frameHeight: 70 });
         this.load.spritesheet('exit', 'https://content.codecademy.com/courses/learn-phaser/Cave%20Crisis/cave_exit.png', { frameWidth: 60, frameHeight: 70 });
-        this.load.spritesheet('coin', 'https://raw.githubusercontent.com/sukichn/sukichn.github.io/refs/heads/main/Resources/css/Images/coin-sprite.png', { frameWidth: 60, frameHeight:  70});
+        this.load.spritesheet('coin', 'https://raw.githubusercontent.com/sukichn/sukichn.github.io/refs/heads/main/Resources/css/Images/coin-sprite.png', { frameWidth: 200, frameHeight: 155});
         this.load.image('leftButton', 'Resources/css/Images/.png'); // Replace with the actual path to your start button image
         this.load.image('upButton', 'Resources/css/Images/.png'); // Replace with the actual path to your start button image
         this.load.image('rightButton', 'Resources/css/Images/.png'); // Replace with the actual path to your start button image
@@ -92,134 +92,153 @@ class GameScene extends BaseScene {
         super.createBackground();
         gameState.active = true;
     
+        // Create platforms and ensure correct positioning
         const platforms = this.physics.add.staticGroup();
         const platPositions = [
-        { x: -550, y: 875 },  // Adjusted y-coordinate
-        { x: -350, y: 875 },    // Adjusted y-coordinate
-        { x: -150, y: 875 },   // Adjusted y-coordinate
-        { x: 150, y: 875 },   // Adjusted y-coordinate
-        { x: 100, y: 680 },   // Adjusted y-coordinate
-        { x: -200, y: 550 },   // Adjusted y-coordinate
-        { x: 350, y: 1075 },
-        { x: 550, y: 875 },    
+            { x: 200, y: 875 },  // Platform 1 starting
+            { x: 300, y: 875 },  // Platform 1 starting
+            { x: 500, y: 875 },  // Platform 2 starting
+            { x: 700, y: 875 },  // Platform 3 starting
+            { x: 900, y: 875 },  // Platform 4 starting
+            { x: 1050, y: 680 }, // Platform 5
+            { x: 750, y: 550 },  // Platform 6
+            { x: 1300, y: 1075 }, // Platform 7
+            { x: 1500, y: 875 },  // Platform 8
         ];
         platPositions.forEach(plat => {
-          platforms.create(plat.x, plat.y, 'platform')
+            platforms.create(plat.x, plat.y, 'platform');
         });
     
-        gameState.player = this.physics.add.sprite(-250, 500, 'codey').setScale(.8);
+        // Create player and ensure it collides with platforms
+        gameState.player = this.physics.add.sprite(200, 700, 'codey').setScale(.8);
         this.physics.add.collider(gameState.player, platforms);
-        
     
+        // Setup cursor keys for player movement
         gameState.cursors = this.input.keyboard.createCursorKeys();
     
+        // Create player animations
         this.anims.create({
-          key: 'run',
-          frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
-          frameRate: 5,
-          repeat: -1
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
         });
-    
-        this.anims.create({
-          key: 'idle',
-          frames: this.anims.generateFrameNumbers('codey', { start: 4, end: 5 }),
-          frameRate: 5,
-          repeat: -1
-        });
-
-       
-    
-        gameState.enemy = this.physics.add.sprite(30, 600, 'snowman');
-        this.physics.add.collider(gameState.enemy, platforms);
     
         this.anims.create({
-          key: 'snowmanAlert',
-          frames: this.anims.generateFrameNumbers('snowman', { start: 0, end: 3 }),
-          frameRate: 4,
-          repeat: -1
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('codey', { start: 4, end: 5 }),
+            frameRate: 5,
+            repeat: -1
         });
     
-        gameState.enemy.anims.play('snowmanAlert', true);
+        // Function to create and position a snowman
+        const createSnowman = (x, y, moveX) => {
+            const snowman = this.physics.add.sprite(x, y, 'snowman');
+            this.physics.add.collider(snowman, platforms);
+            this.anims.create({
+                key: 'snowmanAlert',
+                frames: this.anims.generateFrameNumbers('snowman', { start: 0, end: 3 }),
+                frameRate: 4,
+                repeat: -1
+            });
+            snowman.anims.play('snowmanAlert', true);
     
-        this.physics.add.overlap(gameState.player, gameState.enemy, () => {
-          this.add.text(100, 300, '      Game Over...\n  Click to play again.', { fontFamily: 'Work Sans', fontSize: 36, color: '#ffffff' });
-          this.physics.pause();
-          gameState.active = false;
-          this.anims.pauseAll();
-          gameState.enemy.move.stop();
-          this.input.on('pointerup', () => {
-            this.anims.resumeAll();
-            this.scene.restart();
-          });
+            // Add overlap detection between player and snowman
+            this.physics.add.overlap(gameState.player, snowman, () => {
+                this.add.text(800, 400, '      Game Over...\n  Click to play again.', { fontFamily: 'Work Sans', fontSize: 36, color: '#ffffff' }).setOrigin(0.5);
+                this.physics.pause();
+                gameState.active = false;
+                this.anims.pauseAll();
+                if (snowman.move) snowman.move.stop(); // Stop the movement of the snowman if it has a move property
+                this.input.on('pointerup', () => {
+                    this.anims.resumeAll();
+                    this.scene.restart();
+                });
+            });
+    
+            // Add movement for the snowman if moveX is provided
+            if (moveX) {
+                snowman.move = this.tweens.add({
+                    targets: snowman,
+                    x: moveX,
+                    ease: 'Linear',
+                    duration: 2000,
+                    repeat: -1,
+                    yoyo: true
+                });
+            }
+    
+            return snowman;
+        };
+    
+        // Create snowmen on different platforms
+        gameState.enemy1 = createSnowman(300, 800, 400); // Snowman on Platform 1 with movement
+        gameState.enemy2 = createSnowman(1300, 1005, 1400); // Snowman on Platform 7 with movement
+    
+        // Position the exit on Platform 5
+        gameState.exit = this.physics.add.sprite(700, 130, 'exit');
+        this.anims.create({
+            key: 'glow',
+            frames: this.anims.generateFrameNumbers('exit', { start: 0, end: 5 }),
+            frameRate: 4,
+            repeat: -1
         });
-
-         // Create and animate the coin
-    gameState.coin = this.add.sprite(gameState.enemy.x + 50, gameState.enemy.y - 100, 'snowman'); // Ensure the coin has a physics body
-    this.anims.create({
-        key: 'coinAlert',
-        frames: this.anims.generateFrameNumbers('snowman', { start: 0, end: 3 }),
-        frameRate: 4,
-        repeat: -1
-    });
-    gameState.coin.anims.play('coinAlert', true);
-
-    // Create a non-physics group for the coin
-    gameState.coinGroup = this.add.group();
-    gameState.coinGroup.add(gameState.coin);
-
-    // Add overlap detection between player and coinGroup
-    this.physics.add.overlap(gameState.player, gameState.coinGroup, (player, coin) => {
-        console.log('Player overlapped with the coin'); // Debugging line
-        coin.destroy();
-    }, null, this);
-
-    gameState.exit = this.physics.add.sprite(-250, 142, 'exit');
-    this.anims.create({
-        key: 'glow',
-        frames: this.anims.generateFrameNumbers('exit', { start: 0, end: 5 }),
-        frameRate: 4,
-        repeat: -1
-    });
-    this.physics.add.collider(gameState.exit, platforms);
-    gameState.exit.anims.play('glow', true);
-
-    this.physics.add.overlap(gameState.player, gameState.exit, () => {
-        this.add.text(0, 300, 'You reached the exit!\n  Click to play again.', { fontFamily: 'Work Sans', fontSize: 36, color: '#ffffff' });
-        this.physics.pause();
-        gameState.active = false;
-        this.anims.pauseAll();
-        gameState.enemy.move.stop();
-        this.input.on('pointerup', () => {
-            this.anims.resumeAll();
-            this.scene.restart();
+        gameState.exit.anims.play('glow', true);
+    
+        this.physics.add.collider(gameState.exit, platforms);
+    
+        this.physics.add.overlap(gameState.player, gameState.exit, () => {
+            this.add.text(800, 400, 'You reached the exit!\n  Click to play again.', { fontFamily: 'Work Sans', fontSize: 36, color: '#ffffff' }).setOrigin(0.5);
+            this.physics.pause();
+            gameState.active = false;
+            this.anims.pauseAll();
+            if (gameState.enemy1.move) gameState.enemy1.move.stop();
+            if (gameState.enemy2.move) gameState.enemy2.move.stop();
+            this.input.on('pointerup', () => {
+                this.anims.resumeAll();
+                this.scene.restart();
+            });
         });
-    });
-
-    gameState.enemy.move = this.tweens.add({
-        targets: gameState.enemy,
-        x: 100 + 90,
-        ease: 'Linear',
-        duration: 2000,
-        repeat: -1,
-        yoyo: true,
-        onRepeat: growSnowman
-    });
-        
-        let scaleChange = 1;
-        function growSnowman() {
-          if (scaleChange < 2) {
-            scaleChange += .3;
-            gameState.enemy.setScale(scaleChange);
-            gameState.enemy.y -= 15;
-          }
-          else{
-            scaleChange += 0;
-          }
-        }
-
+    
+        // Create a group for coins
+        gameState.coins = this.physics.add.staticGroup();
+    
+        // Positions for coins on platforms
+        const coinPositions = [
+            { x: 300, y: 825 }, // Coin on Platform 1
+            { x: 500, y: 825 }, // Coin on Platform 2
+            { x: 700, y: 825 }, // Coin on Platform 3
+            { x: 900, y: 825 }, // Coin on Platform 4
+            { x: 1050, y: 630 }, // Coin on Platform 5
+            { x: 1300, y: 1025 }, // Coin on Platform 7
+            { x: 1500, y: 825 }, // Coin on Platform 8
+        ];
+    
+        // Create and position coins
+        coinPositions.forEach(pos => {
+            const coin = this.add.sprite(pos.x, pos.y, 'coin').setScale(0.2);
+            gameState.coins.add(coin);
+        });
+    
+        this.anims.create({
+            key: 'coinAlert',
+            frames: this.anims.generateFrameNumbers('coin', { start: 0, end: 5 }),
+            frameRate: 4,
+            repeat: -1
+        });
+    
+        gameState.coins.getChildren().forEach(coin => {
+            coin.anims.play('coinAlert', true);
+        });
+    
+        // Add overlap detection between player and each coin
+        this.physics.add.overlap(gameState.player, gameState.coins, (player, coin) => {
+            coin.destroy();
+        }, null, this);
+    
         // Set the camera to follow the player only along the x-axis
         this.cameras.main.startFollow(gameState.player, true);
-
+    
         // Touch input settings for left/right press detection
         this.input.on('pointerdown', function (pointer) {
             if (pointer.x < this.scale.width / 2) {
@@ -231,126 +250,44 @@ class GameScene extends BaseScene {
                 gameState.rightPressed = true;
                 gameState.leftPressed = false;
             }
-
+    
             // Store the starting positions for swipe detection
             isClicking = true;
             swipeStartY = pointer.y;
         }, this);
-
+    
         this.input.on('pointerup', function (pointer) {
             gameState.leftPressed = false;
             gameState.rightPressed = false;
-
-            // Capture the end position of the swipe
-            if (isClicking) {
-                let swipeEndY = pointer.y;
-                let deltaY = swipeEndY - swipeStartY;
-
-                if (Math.abs(deltaY) >= 50 && deltaY < 0) {
-                    swipeDirection = 'up';
-                }
-
-                isClicking = false;
-            }
         }, this);
     }
 
     update() {
         // Update background positions for parallax effect
-        gameState.background.tilePositionX += 0.1;
-        gameState.trees.tilePositionX += 0.14;
-        gameState.foreground.tilePositionX += 0.2;
-        gameState.fog.tilePositionX += 0.7;
+        super.updateBackground();
 
         if (gameState.active) {
-            // Touch input settings
-            
-
-            this.input.on('pointermove', function (pointer) {
-                if (pointer.isDown) { // Only process if the pointer is down
-                    // Horizontal movement handling
-                    if (pointer.x < this.scale.width / 2) {
-                        // Left side of the screen
-                        gameState.leftPressed = true;
-                        gameState.rightPressed = false;
-                    } else {
-                        // Right side of the screen
-                        gameState.rightPressed = true;
-                        gameState.leftPressed = false;
-                    }
-            
-                    // Vertical movement handling
-                    if (pointer.y < this.scale.height / 2) {
-                        // Top half of the screen
-                        gameState.upPressed = true;
-                    } else {
-                        // Bottom half of the screen
-                        gameState.upPressed = false;
-                    }
-                }
-            }, this);
-            
-            this.input.on('pointerdown', function (pointer) {
-                // Horizontal movement handling
-                if (pointer.x < this.scale.width / 2) {
-                    // Left side of the screen
-                    gameState.leftPressed = true;
-                    gameState.rightPressed = false;
-                } else {
-                    // Right side of the screen
-                    gameState.rightPressed = true;
-                    gameState.leftPressed = false;
-                }
-            
-                // Vertical movement handling
-                if (pointer.y < this.scale.height / 2) {
-                    // Top half of the screen
-                    gameState.upPressed = true;
-                }
-            }, this);
-            
-            this.input.on('pointerup', function (pointer) {
-                gameState.leftPressed = false;
-                gameState.rightPressed = false;
-                gameState.upPressed = false;
-            }, this);
-        
             // Handle movement based on touch input or keyboard input
             if (gameState.cursors.left.isDown || gameState.leftPressed) {
                 gameState.player.setVelocityX(-360);
                 gameState.player.anims.play('run', true);
-            } else if (gameState.cursors.right.isDown || gameState.rightPressed) {
-                gameState.player.setVelocityX(360);
-                gameState.player.anims.play('run', true);
-            } else {
-                gameState.player.setVelocityX(0);
-                gameState.player.anims.play('idle', true);
-            }
-        
-            if ((gameState.cursors.up.isDown || gameState.upPressed) && gameState.player.body.touching.down) {
-                gameState.player.setVelocityY(-800);
-            }
-    
-            // Handle movement based on swipe gestures or keyboard input
-            if (gameState.cursors.left.isDown || gameState.leftPressed) {
-                gameState.player.setVelocityX(-360);
-                gameState.player.anims.play('run', true);
                 gameState.player.flipX = true;
-    
             } else if (gameState.cursors.right.isDown || gameState.rightPressed) {
                 gameState.player.setVelocityX(360);
                 gameState.player.anims.play('run', true);
                 gameState.player.flipX = false;
-        
             } else {
                 gameState.player.setVelocityX(0);
                 gameState.player.anims.play('idle', true);
             }
-    
+
+            if ((gameState.cursors.up.isDown || gameState.upPressed) && gameState.player.body.touching.down) {
+                gameState.player.setVelocityY(-800);
+            }
 
             // Check if the player has fallen off the page
             if (gameState.player.y > 1200) {
-                this.add.text(900, 900, '      Game over!\nClick to play again.', {
+                this.add.text(800, 500, '      Game over!\nClick to play again.', {
                     fontFamily: 'Work Sans',
                     fontSize: '36px',
                     color: '#ffffff'
