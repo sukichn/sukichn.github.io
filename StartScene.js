7feac429a3859a0c711497d767a723f227b3df01
Suki Chan <sukihchn@gmail.com>
2024-10-12 16:34:42 -0400

    cleaned edges of bg snowscape

--- StartScene.js
+++ StartScene.js
@@ -37,8 +37,8 @@ class StartScene extends Phaser.Scene {
         const fontSize = getResponsiveFontSize();
         this.add.text(800, 250, "The Alchemist's Adventure", { fontSize: fontSize, fill: '#ffffff', fontFamily: 'Work Sans' }).setOrigin(0.5).setDepth(1);
 
-        // Add a start button
-        const startImage = this.add.image(800, 500, 'startImage').setInteractive().setDepth(1);
+        // Add a start image
+        /*const startImage = this.add.image(800, 500, 'startImage').setInteractive().setDepth(1);*/
 
         const startText = this.add.text(800, 750, 'Click to begin...', { fontSize: fontSize, fill: '#ffffff', fontFamily: 'Work Sans' }).setOrigin(0.5).setDepth(1).setInteractive();
         this.input.on('pointerdown', () => {
