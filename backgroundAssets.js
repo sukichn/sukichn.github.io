88c4a44d93f4e979b5e7e77c62ae9d0bfd5d6e32
Suki Chan <sukihchn@gmail.com>
2024-10-14 23:23:46 -0400

    moved background in backgrounAssets down by 20

--- backgroundAssets.js
+++ backgroundAssets.js
@@ -16,7 +16,7 @@ const gameAlert = document.getElementById('game-alert');
             .setOrigin(0, 0)
             .setScrollFactor(0)
             .setScale(1);
-        gameState.background = scene.add.tileSprite(0, 0, scene.cameras.main.width, scene.cameras.main.height, 'bg')
+        gameState.background = scene.add.tileSprite(0, -20, scene.cameras.main.width, scene.cameras.main.height, 'bg')
             .setOrigin(0, 0)
             .setScrollFactor(0)
             .setScale(1);
