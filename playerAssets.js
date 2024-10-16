ae4ad218d5b5962aaa478953204ed8a81e9a7240
Suki Chan <sukihchn@gmail.com>
2024-10-15 23:04:52 -0400

    added 4 frames of character sprite for idle movement

--- playerAssets.js
+++ playerAssets.js
@@ -2,24 +2,24 @@
        // Load codey spritesheet
     global.loadCodeyAssets = function(scene) {
         scene.load.spritesheet('codey', 'https://raw.githubusercontent.com/sukichn/sukichn.github.io/refs/heads/main/Resources/css/Images/character-sprite.png', {
-            frameWidth: 103,
-            frameHeight: 138,
+            frameWidth: 111,
+            frameHeight: 176,
         });
     };
 
     // Create codey animations
     global.createCodeyAnimations = function(scene) {
-        scene.anims.create({
+        /*scene.anims.create({
             key: 'run',
             frames: scene.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
             frameRate: 5,
             repeat: -1
-        });
+        });*/
 
         scene.anims.create({
             key: 'idle',
-            frames: scene.anims.generateFrameNumbers('codey', { start: 4, end: 5 }),
-            frameRate: 5,
+            frames: scene.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+            frameRate: 4,
             repeat: -1
         });
     };
